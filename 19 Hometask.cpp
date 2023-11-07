0e66d90910d2524023ca876efb88f4616ab1d35e
Nikita MIkhailov <nikita57979@gmail.com>
2023-11-07 16:50:17 +0300

    Добавьте файлы проекта.

---
+++ 19 Hometask.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+using namespace std;
+
+class Animals 
+{
+public:
+    virtual void Voice()
+    {
+        cout << "Animal sounds" << endl;
+    }
+};
+
+class Dog : public Animals 
+{
+public:
+    void Voice() override 
+    {
+        cout << "Woof!" << endl;
+    }
+};
+
+class Cat : public Animals 
+{
+public:
+    void Voice() override
+    {
+        cout << "Meow!" << endl;
+    }
+};
+
+class Cow : public Animals 
+{
+public:
+    void Voice() override 
+    {
+        cout << "Moo!" << endl;
+    }
+};
+
+int main() 
+{
+    Animals* animals[3];
+    animals[0] = new Dog();
+    animals[1] = new Cat();
+    animals[2] = new Cow();
+
+    for (int i = 0; i < 3; i++) 
+    {
+        animals[i]->Voice();
+    }
+}
